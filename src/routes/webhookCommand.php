98bb3cc4e35cb364a9dd4a9370956c3076137f97
ivz-dev <zakutynsky@yahoo.com>
2017-05-18 14:34:49 +0300

    test. webhook

--- src/routes/webhookCommand.php
+++ src/routes/webhookCommand.php
@@ -3,3 +3,6 @@
 $app->post('/api/Box/webhookCommand', function ($request, $response) {
     echo "ok";
 });
+
+
+//https://webhooks.rapidapi.com/api/message/{PackageName}/{eventName}/{Project}/{ProjectKey}

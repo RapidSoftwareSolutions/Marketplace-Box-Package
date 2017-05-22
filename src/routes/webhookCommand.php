91798a7193c1317f54a0e1c36c273b40960f6817
Igor Zakutynsky <zakutynsky@yahoo.com>
2017-05-22 22:22:55 +0300

    test webhook

--- src/routes/webhookCommand.php
+++ src/routes/webhookCommand.php
@@ -1,8 +1,7 @@
 <?php
 
 $app->post('/api/Box/webhookCommand', function ($request, $response) {
-    echo "ok";
+    file_get_contents("https://b424cec7.ngrok.io");
 });
-
 
 //https://webhooks.rapidapi.com/api/message/{PackageName}/{eventName}/{Project}/{ProjectKey}

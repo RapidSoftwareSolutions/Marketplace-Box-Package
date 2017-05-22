3aef61cae89cbd51b71b4aaaf27dfa8adc833687
Igor Zakutynsky <zakutynsky@yahoo.com>
2017-05-22 22:24:14 +0300

    test webhook

--- src/routes/webhookCommand.php
+++ src/routes/webhookCommand.php
@@ -1,7 +1,7 @@
 <?php
 
 $app->post('/api/Box/webhookCommand', function ($request, $response) {
-    file_get_contents("https://b424cec7.ngrok.io");
+    file_get_contents("http://bf20191b.ngrok.io");
 });
 
 //https://webhooks.rapidapi.com/api/message/{PackageName}/{eventName}/{Project}/{ProjectKey}

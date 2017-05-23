e39c7d943dde1a59fa2ed4079b360fbd79e78f74
ivz-dev <zakutynsky@yahoo.com>
2017-05-23 12:13:44 +0300

    test

--- src/routes/webhookCommand.php
+++ src/routes/webhookCommand.php
@@ -1,7 +1,7 @@
 <?php
 
 $app->post('/api/Box/webhookCommand', function ($request, $response) {
-    file_get_contents("http://bf20191b.ngrok.io");
+    file_get_contents("http://4a1c0f62.ngrok.io/");
 });
 
 //https://webhooks.rapidapi.com/api/message/{PackageName}/{eventName}/{Project}/{ProjectKey}

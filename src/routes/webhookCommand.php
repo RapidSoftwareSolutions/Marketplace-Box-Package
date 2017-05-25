08108b812f1f3c8941b6ecdddfca8feef2ffca71
ivz-dev <zakutynsky@yahoo.com>
2017-05-25 10:22:35 +0300

    test webhook

--- src/routes/webhookCommand.php
+++ src/routes/webhookCommand.php
@@ -3,7 +3,7 @@
 $app->post('/api/Box/webhookCommand', function ($request, $response) {
 
     $result['callback'] = 'success';
-    $result['contextWrites']['to'] = "Hello";
+    $result['contextWrites']['to'] = json_encode(["text"=>"Hello"]);
 
     return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($result);
 

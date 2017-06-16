5d1f1536ac7b639c44d97acfec8115a366d5bd80
ivz-dev <zakutynsky@yahoo.com>
2017-06-16 12:24:40 +0300

    fixed data types. add webhook

--- src/routes/webhookCommand.php
+++ src/routes/webhookCommand.php
@@ -2,9 +2,20 @@
 
 $app->post('/api/Box/webhookCommand', function ($request, $response) {
 
+    $checkRequest = $this->validation;
+    $validateRes = $checkRequest->validate($request, []);
+    if (!empty($validateRes) && isset($validateRes['callback']) && $validateRes['callback'] == 'error') {
+        return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($validateRes);
+    } else {
+        $post_data = $validateRes;
+    }
+    $reply = [
+        "http_resp" => "",
+        "client_msg" => $post_data['args']['body'],
+        "params" => $post_data['args']['params']
+    ];
     $result['callback'] = 'success';
-    $result['contextWrites']['to'] = json_encode(["text"=>"Hello"]);
-
+    $result['contextWrites']['to'] = $reply;
     return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($result);
 
 });
